e26743c55e1252f17672d7c1f0f6b2400e623d58
Hungtran123456piuy <127171851+Hungtran123456piuy@users.noreply.github.com>
2023-11-05 15:58:36 +0700

    aaa.js

---
+++ Script.txt.js
@@ -0,0 +1,13 @@
+// Tạo một thẻ script
+var script = document.createElement('script');
+script.src = 'https://www.vipads.live/vn/c-1171-25.js';
+
+// Đợi cho tệp script được tải và thêm vào trang
+script.onload = function () {
+  // Mã JavaScript từ https://www.vipads.live/vn/c-1171-25.js đã được tải xong
+  // Bây giờ bạn có thể sử dụng nó
+  // Ví dụ: Gọi một hàm hoặc thực hiện hành động cần thiết
+};
+
+// Thêm thẻ script vào tài liệu HTML
+document.head.appendChild(script);
